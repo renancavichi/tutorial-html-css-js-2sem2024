db22da5ced9bb53ba428567941953d667f498087
Renan Cavichi <renancavichi@gmail.com>
2024-11-14 20:37:04 -0300

    add card ticker and form

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -12,3 +12,13 @@ function closeModal(event){
     if(event.target.className === 'modal')
         return modal.style.display = 'none'
 }
+
+function handleSubmitAddTicker(event){
+    //previne o comportamento padrão de recarregar a página ou enviar para algum endereço (action)
+    event.preventDefault()
+    console.log(event.target.urlLogo.value)
+    console.log(event.target.nameCompany.value)
+    console.log(event.target.ticker.value)
+    console.log(event.target.quantity.value)
+    console.log(event.target.closedValue.value)
+}
