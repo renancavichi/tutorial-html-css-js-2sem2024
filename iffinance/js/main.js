b945109bc95056cbb08e4192d0753301fa026f8f
Renan Cavichi <renancavichi@gmail.com>
2024-11-14 22:13:08 -0300

    add card and reset form

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -13,12 +13,37 @@ function closeModal(event){
         return modal.style.display = 'none'
 }
 
-function handleSubmitAddTicker(event){
+function AddTicker(event){
     //previne o comportamento padrão de recarregar a página ou enviar para algum endereço (action)
+    // ▼
     event.preventDefault()
-    console.log(event.target.urlLogo.value)
-    console.log(event.target.nameCompany.value)
-    console.log(event.target.ticker.value)
-    console.log(event.target.quantity.value)
-    console.log(event.target.closedValue.value)
+
+    const cardList = document.querySelector('#card-list')
+
+    // event.target (form)
+    const urlLogo = event.target.urlLogo.value
+    const nameCompany = event.target.nameCompany.value
+    const ticker = event.target.ticker.value
+    const quantity = event.target.quantity.value
+    const closedValue = event.target.closedValue.value
+
+    cardList.innerHTML += `
+                    <div class="card-ticker">
+                        <header>
+                            <img src="${urlLogo}" alt="Logo">
+                            <h4>${nameCompany}</h4>
+                            <span>${ticker}</span>
+                        </header>
+                        <main>
+                            <p>Valor: <span style="color: #1ebd1e">${closedValue} <span>▲</span></span></p>
+                        </main>
+                        <footer>
+                            <p>Quantidade: <span>${quantity}</span></p>
+                            <p>Posição: <span>R$ ${+quantity * +closedValue}</span></p>
+                        </footer>
+                    </div>
+                `
+    closeModal()
+    event.target.reset()
 }
+
